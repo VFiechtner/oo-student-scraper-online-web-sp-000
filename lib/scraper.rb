36cfaf023c3c721c81fe2b3404ad8dbf7706ca33
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-11-29 23:59:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     html = open(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.student-card").each do |student|
-      student_details = []
+      student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
