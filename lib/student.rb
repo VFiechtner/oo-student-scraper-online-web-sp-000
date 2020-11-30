e42a2f7eaa826898980775f1adeb6929519769fd
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-11-30 01:16:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,21 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each {|k, v| self.send(("#{k}="), v)}
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash|
-      self.new(student_hash)
+    students_array.map do |student_hash|
+      new(student_hash)
     end
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |k, v|
-      self.send(("#{k}="), v)
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
     end
     self
   end
@@ -25,5 +27,4 @@ class Student
   def self.all
     @@all
   end
-
 end
